93e0ea4ee533773b9037415e3f4251768d61677c
Anthony Kroeger <anthony.kroe@gmail.com>
2022-07-09 23:55:30 +1000

    Finished important calculator functionality

--- script.js
+++ script.js
@@ -17,31 +17,90 @@ function divide(num1, num2) {
 function operate(num1, operator, num2) {
   let output = 0;
   switch (operator) {
-    case add:
+    case '+':
       output = add(num1, num2);
       break;
-    case subtract:
+    case '−':
       output = subtract(num1, num2);
       break;
-    case multiply:
+    case '×':
       output = multiply(num1, num2);
       break;
-    case divide:
-      output = divide(num1, num2);
+    case '÷':
+      if (num2 === 0) {
+        alert('Error, cannot divide by 0');
+        clear.click();
+      } else {
+        output = divide(num1, num2);
+      }
       break;
   }
   return output;
 }
 
+let num1 = '';
+let operation = '';
+let num2 = '';
+let result = '';
+
 const display = document.querySelector('#display');
 
-const buttons = document.querySelectorAll('.number');
+const operators = document.querySelectorAll('.operator');
+operators.forEach((operator) => {
+  operator.addEventListener('click', () => {
+    /* Updates display with current result if a 2nd operator is used */
+    if (num2) {
+      result = operate(+num1, operation, +num2);
+      display.textContent = result;
+      num1 = result;
+      num2 = '';
+    } else {
+      num1 = display.textContent;
+    }
+    operation = operator.textContent;
+  })
+});
+
+const numbers = document.querySelectorAll('.number');
+numbers.forEach((number) => {
+  number.addEventListener('click', () => {
+    if (operation) {
+      num2 += number.textContent;
+      display.textContent = num2;
+    } else if (display.textContent === '0') {
+      num1 = number.textContent;
+      display.textContent = num1;
+    } else {
+      num1 += number.textContent;
+      display.textContent = num1;
+    }
+  });
+});
 
 const clear = document.querySelector('.clear');
+clear.addEventListener('click', () => {
+  display.textContent = "0";
+  num1 = '';
+  num2 = '';
+  operation = '';
+  result = '';
+})
 
 const decimal = document.querySelector('.decimal');
-decimal.addEventListener('click', addToDisplay(decimal.textContent));
+decimal.addEventListener('click', () => {
+  if (!display.textContent.includes('.')) {
+    display.textContent += decimal.textContent;
+  }
+});
 
-function addToDisplay(text) {
-  display.textContent += text;
-}
+const equals = document.querySelector('#equals');
+equals.addEventListener('click', () => {
+  if (num1 === '') {
+    display.textContent = '0';
+  } else if (operation === '' || num2 === '') {
+    display.textContent = num1;
+  } else {
+    result = operate(+num1, operation, +num2);
+    display.textContent = result;
+  }
+});
